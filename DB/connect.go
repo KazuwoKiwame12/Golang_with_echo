452d6f26be25481d7589a72d231642d4f6a964a9
SoccerPlay32 <yahoosekainosekai@gmail.com>
2020-08-21 16:59:11 +0900

    heroku用の接続情報

--- DB/connect.go
+++ DB/connect.go
@@ -9,7 +9,8 @@ import (
 )
 
 func Connect() *gorm.DB {
-	connstr := "host=" + os.Getenv("DB_HOST") + " port=" + os.Getenv("DB_PORT") + " user=" + os.Getenv("DB_USER") + " dbname=" + os.Getenv("DB_NAME") + " password=" + os.Getenv("DB_PASSWORD")
+	//connstr := "host=" + os.Getenv("DB_HOST") + " port=" + os.Getenv("DB_PORT") + " user=" + os.Getenv("DB_USER") + " dbname=" + os.Getenv("DB_NAME") + " password=" + os.Getenv("DB_PASSWORD")
+	connstr := os.Getenv("DATABASE_URL")
 	db, err := gorm.Open("postgres", connstr)
 	if err != nil {
 		log.Fatal(err.Error())
